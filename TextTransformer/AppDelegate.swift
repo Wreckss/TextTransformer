13a5c898789d9bccf51e630c58504267b58ef5a4
tacoRiot <46939881+tacoRiot@users.noreply.github.com>
2019-07-28 16:44:11 -0600

    Initial Commit

---
+++ TextTransformer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextTransformer
+//
+//  Created by taco on 7/28/19.
+//  Copyright © 2019 tacoTruck. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TextTransformer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextTransformer
+//
+//  Created by taco on 7/28/19.
+//  Copyright © 2019 tacoTruck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
